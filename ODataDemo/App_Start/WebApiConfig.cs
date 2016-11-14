e3a123e0082acac19e4a82693b724b4d8ea08dbf
EhanAreesha <sahasan_nub@yahoo.com>
2016-11-14 16:07:12 +0600

    first commit

---
+++ ODataDemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using ODataDemo.Models;
+using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+
+
+namespace ODataDemo
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Student>("Students");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+        }
+    }
+}
---
+++ ODataDemo/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace ODataDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
